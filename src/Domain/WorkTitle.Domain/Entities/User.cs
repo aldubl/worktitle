0f60dbd61485011268e05ba57e771be5cd8d3cd6
Владимир Гнатюк <gna.vladimir@gmail.com>
2023-11-20 13:53:58 +0900

    Fix error 42703

--- src/Domain/WorkTitle.Domain/Entities/User.cs
+++ src/Domain/WorkTitle.Domain/Entities/User.cs
@@ -10,18 +10,18 @@ namespace WorkTitle.Domain.Entities
 {
     public sealed class User : BaseEntity
     {
-        public string? Name { get; set; }
+        public required string Name { get; set; }
 
-        public string? Login { get; set; }
+        public required string Login { get; set; }
 
         public string? PhotoUrl { get; set; }
 
-        public string? ChatId { get; set; }
-        public string Email { get; set; }
+        public required string ChatId { get; set; }
+        public required string Email { get; set; }
 
-        public Guid? DefaultListId { get; set; }
+        public required Guid DefaultListId { get; set; }
 
-        public WishList? DefaultList { get; set; }
+        public required WishList? DefaultList { get; set; }
 
         public ICollection<WishList>? Lists { get; set; }
     }
